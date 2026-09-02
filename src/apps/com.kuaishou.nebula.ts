2d7c7b8a241bf2155519849dc25d30f2d39575f5
175cc <@example.com>
2026-09-03 02:10:10 +0800

    快手极速版-奖励

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -5,24 +5,6 @@ export default defineGkdApp({
   name: '快手极速版',
   groups: [
     {
-      key: 100,
-      name: '弹窗广告-瓜分百亿金币',
-      desc: '任务中心弹窗',
-      enable: false,
-      rules: [
-        {
-          key: 0,
-          matches: ['@Image < View + View > [text="瓜分百亿金币"]'],
-          fastQuery: true,
-          snapshotUrls: ['https://i.gkd.li/i/31335133'],
-          activityIds: [
-            'com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity',
-            'com.yxcorp.gifshow.HomeActivity',
-          ],
-        },
-      ],
-    },
-    {
       key: 101,
       name: '弹窗广告-打开推送通知',
       desc: '任务中心弹窗',
@@ -41,16 +23,15 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 102,
-      name: '弹窗广告-限时邀好友',
+      key: 103,
+      name: '弹窗-开宝箱奖励已到账',
       desc: '任务中心弹窗',
       enable: false,
       rules: [
         {
           key: 0,
-          matches: ['@Image < View + View >n [text="去微信邀请好友"]'],
-          fastQuery: true,
-          snapshotUrls: ['https://i.gkd.li/i/31354466'],
+          matches: ['@Image < View + View >n [text="开宝箱奖励已到账"]'],
+          snapshotUrls: ['https://i.gkd.li/i/31750186'],
           activityIds: [
             'com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity',
             'com.yxcorp.gifshow.HomeActivity',
